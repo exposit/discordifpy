993c01fa10df4cb8b21c481cbc1dea2bbeb2e188
exposit <alto.dizi@gmail.com>
2018-12-12 23:38:13 -0800

    initial

--- README.md
+++ README.md
@@ -1,1 +1,74 @@
 # discordifpy
+
+#### What It Is
+
+This is a python script that interfaces between Discord and an interactive fiction interpreter. This allows you to play parser-based interactive fiction games in a Discord channel with friends.
+
+It requires Python and an internet connection; I run it off my computer on demand. Setting it up as an actual remote bot should be doable if you know what you're doing there.
+
+Please remember that this is not thoroughly tested. Some games will work better than others, especially those that have fancy maps and do parser tricks. Not all games will work or be playable even if they do work.
+
+**IMPORTANT Use this script at your own risk. I strongly advise using a VM. And not leaving the bot running 24/7 if you're not comfortable with whatever shenanigans your users might get up to.**
+
+It is built off of [slackifpy](https://github.com/exposit/slackifpy).
+
+#### IF You Know What You Are Doing
+
+Download the repo. I suggest using a VM running linux as host. Install discord.py for python. Download the games you want to play. Get the source for and compile the appropriate interpreter(s) with glk. Update gamedb.py with game info. Make a discord bot. Change the configuration options in the main script as the comments direct, using your channel id and your bot token. The bot wil report in once it is active; use "@ifbot help" in Discord to get a list of commands.
+
+#### Step By Step
+
+This is not a hard or complicated process, but it requires multiple steps and there's potential for confusion at nearly every one of them. Treat each step as a separate task that might require extensive googling. Don't be afraid to google for tutorials or error messages at each step.
+
+0. (optional but recommended) Consider using the instructions for [slackifpy](https://github.com/exposit/slackifpy) as a guide. I haven't tested this with a VM but it should work fine.
+
+1. Download and extract the discordifpy zip. NOTE: The folder structure is pretty arbitrary and easily changed, but I suggest leaving it for now and changing it if you intend to after you've got a sample game up and running.
+
+2. Verify that you have Python. You need Python 3. Install discord.py on the system where you will be running discordif.py. You may need to install pip.
+
+    [https://github.com/Rapptz/discord.py](https://github.com/Rapptz/discord.py)
+
+3. This is the hardest step to explain because there are many variables. You need to download and compile interpreters for each type of IF game you want to run. This requires working with source and the command line.
+
+  ### You don't need all the interpreters, just the ones that handle the games you want to run. This will most likely be [FROTZ](https://github.com/DavidGriffith/frotz).
+
+  **IMPORTANT**: You need to compile from the operating system you will ultimately use to run discordifpy. If any of these steps fail at any point, you're probably missing dependencies. Read the error message and google it.
+
+  Every interpreter has notes and install instructions -- the steps I list here worked for me but there are a lot of variables.
+
+  - **FROTZ:** You ultimately want to compile "dfrotz", which outputs text to the terminal instead of to a fancy window. First, grab the zip from github. Unpack the zip. Anywhere is fine, but the Downloads folder works. READ THE README. Open a terminal in the frotz-master directory you just unpacked. Type "make dumb" and wait for it to finish. You should now have a file named "dfrotz" that wasn't there before. Copy it to the "terps" subfolder.
+
+      https://github.com/DavidGriffith/frotz
+
+  - **GLULXE:** Compile glulxe with cheapglk; this is the default so it's easy. READ THE README. Download both zips and extract. Rename cheapglk-master to cheapglk. Open cheapglk in the terminal and type "./make", then repeat for the glulxe folder.
+
+    https://github.com/erkyrath/glulxe
+    https://github.com/erkyrath/cheapglk
+
+  - **FROBTADS:** This one can be tricky because of dependencies. Download the zip and extract. READ THE README. Go to the FrobTads directory and open a terminal. At the terminal, type "./bootstrap", then "./configure", and finally "make". Be prepared for it to take awhile.
+
+    https://github.com/realnc/frobtads
+
+4. Download a game that's playable by at least one of the interpreters you've compiled. The sample gamedb assumes you've got [9:05 by Adam Cadre](http://ifdb.tads.org/viewgame?id=qzftg3j8nh5f34i2) from the [ifdb.tads.org](ifdb). Unzip it, and drop the 905.z5 file into the "games" subfolder.
+
+5. Open gamedb.py in a text editor. It's already set up for 9:05 and dfrotz. But you can add more games if you'd like. Sample frobTADs and Glulxe game definitions are included for reference. Case matters. The uniqueid can be any string without spaces but should be short and memorable.
+
+  **NOTE:** If you are using frobTads, be sure to put "-i plain" in place of "None" in the arguments field for each TADs game in gamedb.py.
+
+6. Open discordif.py. Change the paths variables to reflect your own path if necessary. Change any other variables.
+
+7. Set up your Discord to work with discordif.py and discordif.py to work with your Discord.
+
+    - Make a dedicated channel for your ifbot to operate in. Ours is called "xintfiction" so it ends up at the bottom of the channel list.
+
+    - Follow the instructions [here](https://github.com/reactiflux/discord-irc/wiki/Creating-a-discord-bot-&-getting-a-token) to get a token.
+
+    - Paste the token into discordif.py in the "token" variable. This token needs to be kept secret. You can alternatively copy that "token = ''" line into a separate file and uncomment the import.
+
+  **NOTE** Once you put your token in, don't share this file anywhere. It will allow malicious people to possibly do malicious things. If you think your token is compromised, reset the bot token on the discord website.
+
+8. Run discordif.py. Navigate to the directory you unzipped discordif.py and run "python discordif.py". The bot should report in. In Discord, type in "@ifbot list". You should see a list of all games you've listed in gamedb. Use "@ifbot help" to get help.
+
+9. If everything works as expected, you're good to go. If the script chokes, read the error messages and address them. If you get a "file not found" issue on launch, check your path variables -- they may need to be more explicit or set for your system.
+
+Good luck!
---
+++ discordif.py
@@ -0,0 +1,265 @@
+#!/usr/bin/python
+# This is discordifpy, a simple discord bot for running IF games. It is not trivial to get working.
+# for more information beyond the readme and a somewhat relevant tutorial please see
+# https://github.com/exposit/slackifpy
+# licensed under MIT as applicable
+# please use with caution as I am no expert
+import discord
+import asyncio
+from discord.ext.commands import Bot
+from discord.ext import commands
+import platform
+import random
+import subprocess
+import threading
+import time
+import sys
+import os
+#from secret import token
+
+if sys.version_info.major >= 3:
+    import queue
+else:
+    import Queue as queue
+    input = raw_input
+
+# put your token here (or put this line in a separate file named "secret.py")
+token = ""
+
+# set paths
+game_path = "./games/"
+terp_path = "./terps/"
+
+# some general messaging from ifbot to Slack
+start_msg = "_is starting the game._\n*..... GAME START .....*\n\n"
+end_msg = "_is shutting the game down._\n*..... GAME END .....*\n"
+default_status = "waiting to play some IF"
+
+# format game output as a code block, true or false. Default is not to.
+format_code_block = False
+
+# now open up the game database and parse it
+with open(os.path.join(game_path, 'games.txt'), 'r') as g:
+    games = g.readlines()
+
+games = [x for x in games if not x == "\n"]
+curr = games[0].strip()
+sorted = {}
+sorted[curr] = {}
+for line in games:
+    if line.startswith('  '):
+        a,b = line.strip().split(':', 1)
+        sorted[curr][a] = b.strip()
+    else:
+        curr = line.strip()
+        sorted[curr] = {}
+
+exclude_list = {}
+for key, value in sorted.items():
+    if not value['title'] == '':
+        exclude_list[key] = value
+
+game_list = {}
+for key, value in exclude_list.items():
+    exists = os.path.isfile(os.path.join(game_path, value['file']))
+    if exists:
+        game_list[key] = value
+
+# actual bot starts here
+client = Bot(description="IFBot", command_prefix=commands.when_mentioned_or("!"), pm_help = False)
+
+client.remove_command('help')
+
+@client.event
+async def on_ready():
+    print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | Connected to '+str(len(client.servers))+' servers | Connected to '+str(len(set(client.get_all_members())))+' users')
+    print('--------')
+    return await client.change_presence(game=discord.Game(name=default_status))
+
+# basic functions
+def read_stdout(stdout, q):
+    it = iter(lambda: stdout.read(1), b'')
+    for c in it:
+        q.put(c)
+        if stdout.closed:
+            break
+
+def get_stdout(q, encoding='latin-1'):
+    out = []
+    while 1:
+        try:
+            out.append(q.get(timeout=0.2))
+        except queue.Empty:
+            break
+    return b''.join(out).rstrip().decode('latin-1')
+
+def printout(q):
+
+    send_msg = get_stdout(q)
+
+    if send_msg:
+        # format messaging to strip out some parser cruft
+        # could add more catches if necessary
+        send_msg = send_msg.replace("> >", "")
+
+        if format_code_block:
+            send_msg = "```" + send_msg + "```"
+
+        # print('[GAME]:\n%s' % send_msg)
+
+        return send_msg
+
+# basic routines
+def start_game(game_index):
+
+    game_data = game_list[game_index]
+    game_file = game_data["file"]
+    interpreter = game_data["interpreter"]
+    if game_data["args"] != "None":
+        arguments = game_data["args"].split(" ")
+
+    print('\n[Status] ' + start_msg)
+
+    curr_terp = terp_path + interpreter
+    curr_game = game_path + game_file
+
+    if game_data["args"] != "None":
+        ARGS = [curr_terp, arguments[0], arguments[1], curr_game]
+    else:
+        ARGS = [curr_terp, curr_game]
+
+    # okay, actually start things
+    global gam
+    global q
+
+    gam = subprocess.Popen(ARGS, bufsize=0, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    q = queue.Queue()
+
+    outthread = threading.Thread(target=read_stdout, args=(gam.stdout, q))
+    outthread.daemon = True
+    outthread.start()
+
+    return start_msg + printout(q)
+
+# commands trapped for
+@client.command()
+async def details(*args):
+
+    target_game = args[0]
+
+    if target_game in game_list.keys():
+        result = game_list[target_game]
+        key = target_game
+        title = '**%s** [%s] \n' % (result['title'], key)
+        author = '_%s_\n' % result['author']
+        genre = '%s\n' % result['genre']
+        final = '%s %s %s "%s"' % (title, author, genre, result["blurb"])
+    else:
+        final = "I'm sorry, which game was that again? Use _@ifbot list_ to list all available games."
+
+    await client.say(final)
+    await asyncio.sleep(1)
+
+@client.command()
+async def help(*args):
+
+    result = "To start a game, type in _@ifbot launch <game id>_. To see a list of games and id codes, type _@ifbot list_. To see details about a game, type _@ifbot details <id code>_\n\nTo interact with a game, preface commands with an arrow and a space. \"> look\" or \"> examine\" are good places to start. Most games also have a \"> help\" command.\n\nPlease note that games are referred to by a short code, not by full name.\n"
+
+    await client.say(result)
+    await asyncio.sleep(1)
+
+@client.command()
+async def launch(*args):
+
+    global gam
+    global q
+
+    try:
+        poll = gam.poll()
+        if poll == None:
+            game_active = True
+        else:
+            game_active = False
+    except:
+        game_active = False
+
+    if not game_active:
+        target_game = args[0]
+        if target_game in game_list.keys():
+            result = start_game(target_game)
+            title = game_list[target_game]['title']
+            await client.change_presence(game=discord.Game(name=title))
+        else:
+            result = "I'm sorry, which game was that again? Use _@ifbot list_ to list all available games."
+    else:
+        result = "A game is currently in progress. Please use _> quit_ to exit first."
+
+    await client.say(result)
+    await asyncio.sleep(1)
+
+@client.command()
+async def list(*args):
+
+    answer = ""
+    for key in game_list:
+        curr = game_list[key]
+        short =  "%s..." % curr['blurb'][:98]
+        adln = "**%s** [%s] (%s) %s \n" % (curr['title'], key, curr['genre'], short)
+        answer = answer + adln
+
+    await client.say(answer)
+    await asyncio.sleep(1)
+
+@client.event
+async def on_message(message):
+
+    global gam
+    global q
+
+    # this is a command to pass to the parser
+    if message.content.startswith('> '):
+
+        newMessage = message.content[:]
+
+        command = newMessage[2:]
+
+        try:
+            poll = gam.poll()
+            if poll == None:
+                result = "A game is running."
+            else:
+                result = "No game is running. Try _@ifbot help_."
+        except:
+            result = "No game is running. Try _@ifbot help_."
+
+        if not result.startswith("No game is running"):
+
+            if command:
+                command = (command + '\n').encode('latin-1')
+                gam.stdin.write(command)
+
+                result = printout(q)
+
+        if result == None:
+            result = end_msg
+            print(end_msg)
+            await client.change_presence(game=discord.Game(name=default_status))
+
+        await client.send_message(message.channel, result)
+        await asyncio.sleep(3)
+
+    else:
+        await client.process_commands(message)
+
+@client.event
+async def on_command_error(error, ctx):
+
+    print(error, ctx)
+
+    # default response if no other queries are caught
+    answer = "Please ask me for help with _@ifbot help_."
+
+    await client.send_message(ctx.message.channel, answer)
+    #await client.send_message(client.get_channel(channel), answer)
+
+client.run(token)
